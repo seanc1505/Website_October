5ba5036661bb6b6a66dae198c7376395a9f20f2f
seanc1505 <54627825+seanc1505@users.noreply.github.com>
2021-10-01 00:16:59 +0100

    Adding navbar

--- phoenix_academy_website/settings.py
+++ phoenix_academy_website/settings.py
@@ -11,10 +11,10 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+PROJECT_DIR = os.path.join(BASE_DIR, "phoenix_academy_website")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'phoenix_academy_website.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(PROJECT_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- phoenix_academy_website/urls.py
+++ phoenix_academy_website/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from django.views.generic.base import TemplateView
+from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',TemplateView.as_view(template_name = "index.html"), name= "index"),
+    path('about',TemplateView.as_view(template_name = "about.html"), name= "about"),
+    path('contact',views.contact, name= "contact")
 ]
